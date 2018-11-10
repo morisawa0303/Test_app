6d4b0136a5d80d1ca0df5974bdfa1ab6dba51752
DESKTOP-47135VN\morisawa <lukaluka739@gmail.com>
2018-11-10 20:18:32 +0900

    [fix]userデータベースへの登録処理追加、パスワードのテキストフィールドをパスワードフィールドに変更

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,15 @@ class UserController < ApplicationController
   def new
     @user=User.new
   end
+
+
+  def create
+    @user=User.new(params.require(:user).permit(:name,:mail,:pass))
+    if @user.save
+      flash[:notice]="新規登録しました"
+      redirect_to("/")
+    else
+      redirect_to("/user/signup")
+    end
+  end
 end
