f88a6eabbbe468f6876ffefc5895c9bc4ae4806f
DESKTOP-47135VN\morisawa <lukaluka739@gmail.com>
2018-11-12 20:06:41 +0900

    [fix]投稿機能作成

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,7 +1,20 @@
 class PostController < ApplicationController
   def new
+    @post=Post.new
   end
 
   def index
+    @post=Post.all
+  end
+
+  def create
+    @post=Post.new(content: get_post_params[:content])  
+    if @post.save
+      redirect_to('/post')
+    end
+  end
+
+  def get_post_params
+    params.require(:post).permit(:content)
   end
 end
