61b63d2a13327f825998c6c95715f8eacb434127
DESKTOP-47135VN\morisawa <lukaluka739@gmail.com>
2018-11-13 22:35:53 +0900

    [fix]バリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
     mount_uploader :image_path, ImageUploader
     belongs_to :user
+
+    validates :content,{presence: true, length: {maximum:140}}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_many :posts
+
+    validates :name,{presence: true}
+    validates :mail,{presence: true}
+    validates :pass,{presence: true}
 end
