b5d9875f5b511e72800fa93b3a73b7a1f9d217c9
DESKTOP-47135VN\morisawa <lukaluka739@gmail.com>
2018-11-13 14:20:15 +0900

    [fix]マイページの時間表示と投稿順の変更

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -29,7 +29,7 @@ class UserController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.posts.order(created_at: :desc)
   end
 
 
